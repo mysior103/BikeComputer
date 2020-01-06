2fc0fb470f2a4ff3b3e213a1d0ef200e9ce64a76
mysior103 <mysior13@op.pl>
2020-01-06 22:25:03 +0100

    init commit

---
+++ Src/DEV_Config.c
@@ -0,0 +1,55 @@
+/******************************************************************************
+**************************Hardware interface layer*****************************
+* | file      	:	DEV_Config.c
+* |	version		:	V1.0
+* | date		:	2017-08-14
+* | function	:	
+	Provide the hardware underlying interface	
+******************************************************************************/
+#include <DEV_Config.h>
+#include "stm32l0xx_hal_spi.h"
+#include "main.h"
+#include <stdio.h>		//printf()
+#include <string.h>
+#include <stdlib.h>
+
+/********************************************************************************
+function:	System Init
+note:
+	Initialize the communication method
+********************************************************************************/
+uint8_t System_Init(void)
+{
+    printf("USE 4wire spi\r\n");
+    return 0;
+}
+
+void System_Exit(void)
+{
+
+}
+/********************************************************************************
+function:	Hardware interface
+	SPI4W_Write_Byte(value) : 
+		HAL library hardware SPI
+********************************************************************************/
+void SPI4W_Write_Byte(uint8_t value)
+{
+    HAL_SPI_Transmit(&hspi1, &value, 1, 500);
+}
+
+/********************************************************************************
+function:	Delay function
+note:
+	Driver_Delay_ms(xms) : Delay x ms
+	Driver_Delay_us(xus) : Delay x us
+********************************************************************************/
+void Driver_Delay_ms(uint32_t xms)
+{
+    HAL_Delay(xms);
+}
+
+void Driver_Delay_us(uint32_t xus)
+{
+    for(int j=xus; j > 0; j--);
+}
